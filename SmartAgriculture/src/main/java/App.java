95c92c8a2660fb92b508c11a96cc43bb8a181ca0
Deniz Onus <deniz.onus@yahoo.de>
2017-08-02 18:23:38 -0400

    WetterInfos können in App.java ausgelsen werden

--- SmartAgriculture/src/main/java/App.java
+++ SmartAgriculture/src/main/java/App.java
@@ -1,12 +1,16 @@
 import static spark.Spark.post;
 import static spark.Spark.staticFiles;
 
+import org.apache.spark.api.java.JavaPairRDD;
+
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
 public class App {
+	private static WetterInfo wetterInfoForGridID = null;
 
 	public static void main(String[] args) {
+		
 		
 		staticFiles.externalLocation("src/main/resources/public");
 
@@ -17,6 +21,12 @@ public class App {
 			double centerPointLat = parsed.read("$.centerPolResult[0]", Double.class);			
 			double centerPointLng = parsed.read("$.centerPolResult[1]", Double.class);
 			
+			int gridId = 1;
+			wetterInfoForGridID = GetWeatherRows.getWetterInfoForGridID(gridId);
+			
+//			System.out.println(wetterInfoForGridID.minTemp);
+			
+			//return wetterInfoForGridID.minTemp;
 			//Martin an browser ein json zurück geben
 			
 			return "";
---
+++ SmartAgriculture/src/main/java/WetterInfo.java
@@ -0,0 +1,20 @@
+import java.io.Serializable;
+import java.time.LocalDate;
+
+// CSV: ID, datum, lat, long, maxTemp, minTemp, Regenwahrscheinlichkeit in %
+public class WetterInfo implements Serializable {
+	@Override
+	public String toString() {
+		return gridKey + "," + datum + "," + lat + "," + lon + "," + maxTemp + "," + minTemp + ","
+				+ regenWahrscheinlichkeit + "," + resultDatum;
+	}
+
+	public String gridKey; // ID aus CSV
+	public String datum;
+	public double lat;
+	public double lon;
+	public double maxTemp;
+	public double minTemp;
+	public double regenWahrscheinlichkeit;
+	public LocalDate resultDatum;
+}
